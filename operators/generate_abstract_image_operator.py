9d3cc61ddac6b965b0d2412769002141256dcc28
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-08-25 21:08:01 +0200

    Refactor DAG implementation

--- operators/generate_abstract_image_operator.py
+++ operators/generate_abstract_image_operator.py
@@ -4,20 +4,41 @@ from airflow.utils.decorators import apply_defaults
 from PIL import Image, ImageDraw
 import hashlib
 import random
+from pymongo import MongoClient
+import gridfs
+import io
+import logging
 
 class GenerateAbstractImageOperator(BaseOperator):
     @apply_defaults
     def __init__(
         self,
-        song_text,
-        output_image_path,
+        mongo_uri,
+        mongo_db,
+        mongo_db_collection,
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.song_text = song_text
-        self.output_image_path = output_image_path
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.mongo_db_collection = mongo_db_collection
 
     def execute(self, context):
+        logging.info("Starting execution of GenerateAbstractImageOperator")
+
+        # Retrieve melody_id from the previous task using XCom
+        melody_id = context['task_instance'].xcom_pull(task_ids='generate_voice_task')['melody_id']
+        logging.info(f"Retrieved melody_id: {melody_id}")
+
+        # Connect to MongoDB and retrieve song text
+        with MongoClient(self.mongo_uri) as client:
+            db = client[self.mongo_db]
+            fs = gridfs.GridFS(db, collection=self.mongo_db_collection)
+
+            melody_info = fs.find_one({"_id": melody_id})
+            song_text = melody_info.get("song_text")
+            logging.info(f"Retrieved song text for melody_id: {melody_id}")
+
         # Define image dimensions
         image_size = (800, 600)
 
@@ -26,7 +47,7 @@ class GenerateAbstractImageOperator(BaseOperator):
         draw = ImageDraw.Draw(image)
 
         # Use a hash of the song text to seed the random generation
-        text_hash = int(hashlib.md5(self.song_text.encode()).hexdigest(), 16)
+        text_hash = int(hashlib.md5(song_text.encode()).hexdigest(), 16)
         random.seed(text_hash)
 
         # Generate abstract patterns with various shapes and colors
@@ -47,5 +68,19 @@ class GenerateAbstractImageOperator(BaseOperator):
                 end_y = random.randint(0, image_size[1])
                 draw.line([(x, y), (end_x, end_y)], fill=color, width=2)
 
-        # Save the generated abstract image
-        image.save(self.output_image_path)
+        # Convert image to bytes
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format="PNG")
+        image_bytes.seek(0)
+
+        # Store the generated abstract image in MongoDB using GridFS
+        with MongoClient(self.mongo_uri) as client:
+            db = client[self.mongo_db]
+            fs = gridfs.GridFS(db, collection='melodies')
+
+            abstract_image_id = fs.put(image_bytes, filename=f"{melody_id}_abstract_image.png", content_type="image/png")
+            logging.info(f"Abstract image stored in MongoDB with ID: {abstract_image_id}")
+
+        logging.info("GenerateAbstractImageOperator execution completed")
+
+        return {"melody_id": str(melody_id), "abstract_image_id": str(abstract_image_id)}
