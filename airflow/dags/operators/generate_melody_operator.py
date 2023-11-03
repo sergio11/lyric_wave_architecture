f5b7323d0395418a8a985a2ea277646e9be18fa1
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-03 20:20:00 +0100

    Implement several improvements in Generate melody operator

--- airflow/dags/operators/generate_melody_operator.py
+++ airflow/dags/operators/generate_melody_operator.py
@@ -9,12 +9,11 @@ import scipy
 class GenerateMelodyOperator(BaseCustomOperator):
 
     """
-    Custom Airflow operator for generating melodies based on text input using Magenta's MusicVAE.
+    Custom Airflow operator for generating musical melodies based on text input using AudioCraft by Facebook.
 
-    This operator downloads a pre-trained Magenta MusicVAE model checkpoint, encodes a given
-    text input into a musical melody, generates the melody, and stores it as a MIDI file
-    in a MinIO bucket. It also updates the metadata in a MongoDB collection with the path
-    to the generated MIDI file.
+    This operator leverages a pre-trained model from Facebook's AudioCraft, encodes a given text input into a musical melody, 
+    generates the melody, and stores it as a WAV audio file in a MinIO bucket. 
+    It also updates the metadata in a MongoDB collection with the path to the generated audio file.
 
     :param mongo_uri: The MongoDB connection URI.
     :param mongo_db: The name of the MongoDB database.
@@ -22,7 +21,7 @@ class GenerateMelodyOperator(BaseCustomOperator):
     :param minio_endpoint: The MinIO server endpoint.
     :param minio_access_key: The access key for MinIO.
     :param minio_secret_key: The secret key for MinIO.
-    :param minio_bucket_name: The name of the MinIO bucket to store generated MIDI files.
+    :param minio_bucket_name: The name of the MinIO bucket to store generated audio files.
 
     The operator is designed to be used within Airflow DAGs for music generation tasks.
     """
@@ -36,26 +35,17 @@ class GenerateMelodyOperator(BaseCustomOperator):
 
     def _generate_melody(self, song_info_id, song_text):
         """
-        Generate a musical melody from the given song text using a pre-trained Magenta MusicVAE model.
+        Generates a musical melody from the given song text using the AudioCraft by Facebook model.
 
-        This private method takes the provided song text, processes it using a pre-trained
-        MusicVAE model, and generates a musical melody. The generated melody is saved as a WAV file.
+        This function use the AudioCraft model to encode the provided song text into a musical melody.
+        The generated melody is saved as a WAV audio file with a unique filename based on the song_info_id.
 
-        :param song_info_id: The unique identifier associated with the song information.
-        :param song_text: The input text used for melody generation.
-        
-        :return: The file path of the generated WAV file.
-        
-        This function uses the 'facebook/musicgen-small' model to generate the melody. It pads
-        the input text, returns the tensor representation, and generates the audio values. The
-        resulting melody is saved as a WAV file with a file name derived from 'song_info_id'.
+        Args:
+            song_info_id (str): The unique identifier for the song information.
+            song_text (str): The text input used for generating the musical melody.
 
-        Example:
-        ::
-        
-            melody_path = _generate_melody(song_id, "Once upon a time in a distant land...")
-            # melody_path could be something like '605c1b6d20095cd4338d12c7.wav'
-
+        Returns:
+            str: The file path to the generated WAV audio file.
         """
         transformers = importlib.import_module("transformers")
         processor = transformers.AutoProcessor.from_pretrained("facebook/musicgen-small")
@@ -72,6 +62,24 @@ class GenerateMelodyOperator(BaseCustomOperator):
         return wav_file_path
 
     def execute(self, context):
+        """
+        Executes the GenerateMelodyOperator to generate and store a melody based on provided song information.
+
+        Args:
+            context (dict): The Airflow task context.
+
+        Raises:
+            Exception: If there's an error during the execution.
+
+        This method is responsible for generating a melody based on song information, storing it in MinIO, and updating the song's
+        document in MongoDB with the path to the generated melody. It performs several steps and handles errors appropriately.
+
+        Args:
+            context (dict): The Airflow task context containing information related to the task execution.
+
+        Returns:
+            dict: A dictionary containing information about the generated melody, specifically the melody's ID.
+        """
         self._log_to_mongodb("Starting execution of GenerateMelodyOperator", context, "INFO")
 
         # Get the configuration passed to the DAG from the execution context
@@ -92,58 +100,35 @@ class GenerateMelodyOperator(BaseCustomOperator):
             error_message = f"Song info with ID {song_info_id} not found in MongoDB"
             self._log_to_mongodb(error_message, context, "ERROR")
             raise Exception(error_message)
-        
+
         self._log_to_mongodb(f"Retrieved song info from MongoDB: {song_info}", context, "INFO")
         # Retrieve song title, text, and description from song_info
         song_title = song_info.get('song_title')
         song_text = song_info.get('song_text')
 
-        # Get MinIO client
-        minio_client = self._get_minio_client(context);
-
         try:
             self._log_to_mongodb("Generating melody...", context, "INFO")
-            wav_file_path = self._generate_melody(song_info_id, song_text)
+            melody_file_path = self._generate_melody(song_info_id, song_text)
             self._log_to_mongodb("Melody generated successfully", context, "INFO")
         except Exception as e:
             error_message = f"An error occurred while generating the melody: {e}"
             self._log_to_mongodb(error_message, context, "ERROR")
             raise Exception(error_message)
-        
+
         self._log_to_mongodb(f"Storing melody in MinIO for '{song_title}'", context, "INFO")
 
-        try:
-            with open(wav_file_path, 'rb') as file_data:
-                file_data.seek(0, 2)  # Ir al final del archivo para obtener el tamaño en bytes
-                file_size_bytes = file_data.tell()
-                file_data.seek(0)  # Volver al principio
-                
-                if file_size_bytes == 0:
-                    error_message = "WAV file is empty"
-                    self._log_to_mongodb(error_message, context, "ERROR")
-                    raise Exception(error_message)
-                
-                file_size_kb = file_size_bytes / 1024
-                self._log_to_mongodb(f"Try to store Melody file ({file_size_kb:.2f} KB) in MinIO bucket: {self.minio_bucket_name}", context, "INFO")
-                
-                minio_client.put_object(
-                    self.minio_bucket_name,
-                    wav_file_path,
-                    file_data,
-                    file_size_bytes,
-                    content_type="audio/wav"
-                )
-                self._log_to_mongodb(f"Melody file stored in MinIO bucket: {self.minio_bucket_name}", context, "INFO")
-        except Exception as e:
-            error_message = f"Error storing WAV file in MinIO: {e}"
-            self._log_to_mongodb(error_message, context, "ERROR")
-            raise Exception(error_message)
+        # Store the generated .wav file in MinIO
+        self._store_file_in_minio(
+            local_file_path=melody_file_path, 
+            minio_object_name=melody_file_path,
+            context=context, 
+            content_type="audio/wav")
 
         # Update the existing BSON document with the path to the WAV file in MinIO
-        song_info['melody_wav_file_path'] = wav_file_path
- 
+        song_info['melody_file_path'] = melody_file_path
+
         # Update the document in MongoDB
-        collection.update_one({"_id": song_info['_id']}, {"$set": song_info})
+        collection.update_one({"_id": song_info_id}, {"$set": song_info})
 
         self._log_to_mongodb(f"Generated melody saved in MongoDB with ID: {song_info_id}", context, "INFO")
         self._log_to_mongodb("GenerateMelodyOperator execution completed", context, "INFO")
