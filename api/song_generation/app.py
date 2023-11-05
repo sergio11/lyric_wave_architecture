c6e85c9c8e414b939af666268d6b96ff6398064b
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-05 20:03:42 +0100

    Improvements in Song Generation and Streaming APIs

--- api/song_generation/app.py
+++ api/song_generation/app.py
@@ -29,6 +29,8 @@ API_EXECUTOR_PASSWORD = os.environ.get("API_EXECUTOR_PASSWORD")
 ELASTICSEARCH_HOST = os.environ.get("ELASTICSEARCH_HOST")
 ELASTICSEARCH_INDEX = os.environ.get("ELASTICSEARCH_INDEX")
 
+LYRIC_WAVE_STREAMING_SERVICE_URL = os.environ.get("LYRIC_WAVE_STREAMING_SERVICE_URL")
+
 elasticsearch_client = Elasticsearch(ELASTICSEARCH_HOST)
 
 # Connect to MongoDB using the provided URI
@@ -53,17 +55,13 @@ def get_music_styles():
         # Retrieve the list of music styles from the collection
         music_styles_cursor = db['music_styles'].find({})
         styles = [{"style_id": str(style['_id']), "style_name": style['style']} for style in music_styles_cursor]
-        response_data = {
-            "status": "success",
-            "code": 200,
-            "message": "Music styles retrieved successfully." if styles else "No music styles found.",
-            "music_styles": styles
-        }
-        return jsonify(response_data), 200
+        response_data = _create_response("success", 200, "Music styles retrieved successfully." if styles else "No music styles found.", {"music_styles": styles})
+        return response_data
     except Exception as e:
         # Handle any exceptions and log errors
         logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
 
 @app.route('/music_styles', methods=['PUT'])
 def update_music_styles():
@@ -81,30 +79,39 @@ def update_music_styles():
                 result = collection.insert_one({"style": style})
                 inserted_ids.append(str(result.inserted_id))
 
-            response_data = {
-                "status": "success",
-                "code": 200,
-                "message": "Music styles updated successfully.",
-                "inserted_ids": inserted_ids
-            }
-            return jsonify(response_data), 200
+            response_data = _create_response("success", 200, "Music styles updated successfully", {"inserted_ids": inserted_ids})
+            return response_data
         else:
-            response_data = {
-                "status": "error",
-                "code": 400,
-                "message": "Invalid or missing 'styles' parameter in the request.",
-            }
-            return jsonify(response_data), 400
+            response_data = _create_response("error", 400, "Invalid or missing 'styles' parameter in the request")
+            return response_data
     except Exception as e:
         # Handle any exceptions and log errors
         logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
+    
+
+# API endpoint for retrieving a song by ID
+@app.route('/songs/<string:song_id>', methods=['GET'])
+def get_song_by_id(song_id):
+    try:
+        song_info = songs_collection.find_one({"_id": ObjectId(song_id)})
+        if song_info:
+            song_data = _get_song_info_with_urls(song_info)
+            response_data = _create_response("success", 200, "Song retrieved successfully", song_data)
+            return response_data
+        else:
+            response_data = _create_response("error", 404, "Song not found")
+            return response_data
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
 
 # API endpoint for generating a song
 @app.route('/generate_song', methods=['POST'])
 def generate_song():
     logger.info("Received a request to generate a song.")
-
     try:
         # Get song title, text, description, keywords, and style ID from the request's JSON data
         song_title = request.json.get('title')
@@ -116,21 +123,22 @@ def generate_song():
         # Validate the length of song_text
         max_length = 200
         if len(song_text) > max_length:
-            return jsonify({"message": "Song text exceeds the maximum allowed length (200 characters)."}), 400
+            return _create_response("error", 400, "Song text exceeds the maximum allowed length (200 characters).")
 
         # Check if a song with the same title already exists
         existing_song = songs_collection.find_one({"song_title": song_title})
         if existing_song:
-            return jsonify({"message": "A song with the same title already exists."}), 400
+            return _create_response("error", 400, "A song with the same title already exists.")
 
         # Check if the provided style_id exists in the music_styles collection
         try:
-            style_id = ObjectId(style_id)
+            style_id = ObjectId(music_style_id)
         except Exception:
-            return jsonify({"message": "Invalid music style ID format. Must be a valid ObjectId."}), 400
+            return _create_response("error", 400, "Invalid music style ID format. Must be a valid ObjectId.")
+
         music_style = music_style_collection.find_one({"_id": style_id})
         if not music_style:
-            return jsonify({"message": "Invalid music style ID. The specified style does not exist."}), 400
+            return _create_response("error", 400, "Invalid music style ID. The specified style does not exist.")
 
         if song_title and song_text:
             logger.info(f"Generating song for '{song_title}' with description: {description}")
@@ -197,88 +205,26 @@ def generate_song():
                 )
 
                 logger.info("DAG execution triggered successfully")
-                response_data = {
-                    "status": "success",
-                    "code": 200,
-                    "message": "Song generated and scheduled successfully.",
-                    "song_info": {
-                        "song_title": song_title,
-                        "song_text": song_text,
-                        "description": description,
-                        "keywords": keywords,
-                        "music_style_id": music_style_id,
-                        "song_info_id": str(song_info_id),
-                        "planned_date": logical_date_str
-                    }
-                }
-                return jsonify(response_data), 200
+                song_data = _get_song_info_with_urls(song_info)
+                response_data = _create_response("success", 200, "Song generated and scheduled successfully.", {"song_info": song_data})
+                return response_data, 200
             else:
                 # If DAG execution failed, remove the document from MongoDB
                 songs_collection.delete_one({"_id": song_info_id})
                 logger.error(f"Error triggering DAG execution: {response.text}")
                 logger.error(f"HTTP Request Headers: {headers}")  # Log the request headers
                 logger.error(f"HTTP Request Body: {dag_run_conf}")  # Log the request body
-                response_data = {
-                    "status": "error",
-                    "code": response.status_code,
-                    "message": "Error triggering DAG execution.",
-                    "song_info": None
-                }
-                return jsonify(response_data), 500
+                response_data = _create_response("error", response.status_code, "Error triggering DAG execution.")
+                return response_data, 500
         else:
             logger.error("Missing title or text parameters")
-            response_data = {
-                "status": "error",
-                "code": 400,
-                "message": "Missing title or text parameters.",
-                "song_info": None
-            }
-            return jsonify(response_data), 400
+            response_data = _create_response("error", 400, "Missing title or text parameters.")
+            return response_data, 400
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
-        response_data = {
-            "status": "error",
-            "code": 500,
-            "message": "An internal server error occurred.",
-            "song_info": None
-        }
-        return jsonify(response_data), 500
+        response_data = _create_response("error", 500, "An internal server error occurred.")
+        return response_data, 500
 
-# API endpoint for retrieving a song by ID
-@app.route('/songs/<string:song_id>', methods=['GET'])
-def get_song_by_id(song_id):
-    try:
-        song_info = songs_collection.find_one({"_id": ObjectId(song_id)})
-        if song_info:
-            music_style_id = song_info.get("music_style_id")
-            style_info = music_style_collection.find_one({"_id": ObjectId(music_style_id)})
-            music_style_name = style_info.get("style_name") if style_info else "Unknown"
-            response_data = {
-                "status": "success",
-                "code": 200,
-                "message": "Song retrieved successfully.",
-                "song_info": {
-                    "song_info_id": str(song_info["_id"]),
-                    "song_title": song_info["song_title"],
-                    "song_text": song_info["song_text"],
-                    "description": song_info.get("description", ""),
-                    "keywords": song_info.get("keywords", ""),
-                    "planned_date": song_info.get("logical_date", ""),
-                    "music_style": music_style_name
-                }
-            }
-            return jsonify(response_data), 200
-        else:
-            response_data = {
-                "status": "error",
-                "code": 404,
-                "message": "Song not found",
-                "song_info": None
-            }
-            return jsonify(response_data), 404
-    except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
 
 # API endpoint for listing all songs paginated, descending by date
 @app.route('/songs', methods=['GET'])
@@ -286,71 +232,41 @@ def list_songs():
     try:
         page = int(request.args.get('page', 1))
         per_page = int(request.args.get('per_page', 10))
-        
+
         songs = list(songs_collection.find().sort([("logical_date", -1)]).skip((page - 1) * per_page).limit(per_page))
         if songs:
-            response_data = {
-                "status": "success",
-                "code": 200,
-                "message": "Songs retrieved successfully.",
-                "songs": [
-                    {
-                        "song_id": str(song["_id"]),
-                        "song_title": song["song_title"],
-                        "song_text": song["song_text"],
-                        "keywords": song.get("keywords", ""),
-                        "description": song.get("description", ""),
-                        "planned_date": song.get("logical_date", "")
-                    }
-                    for song in songs
-                ]
-            }
-            return jsonify(response_data), 200
+            song_list = []
+            for song in songs:
+                song_data = _get_song_info_with_urls(song)
+                song_list.append(song_data)
+            response_data = _create_response("success", 200, "Songs retrieved successfully.", {"songs": song_list})
+            return response_data
         else:
-            response_data = {
-                "status": "error",
-                "code": 404,
-                "message": "No songs found",
-                "songs": []
-            }
-            return jsonify(response_data), 404
+            response_data = _create_response("error", 404, "No songs found", {"songs": []})
+            return response_data
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
 
-# API endpoint for deleting a song by ID
+
 @app.route('/songs/<string:song_id>', methods=['DELETE'])
 def delete_song_by_id(song_id):
     try:
         song_info = songs_collection.find_one({"_id": ObjectId(song_id)})
         if song_info:
             songs_collection.delete_one({"_id": ObjectId(song_id)})
-            response_data = {
-                "status": "success",
-                "code": 200,
-                "message": "Song deleted successfully",
-                "song_info": {
-                    "song_id": str(song_info["_id"]),
-                    "song_title": song_info["song_title"],
-                    "song_text": song_info["song_text"],
-                    "description": song_info.get("description", ""),
-                    "planned_date": song_info.get("logical_date", "")
-                }
-            }
-            return jsonify(response_data), 200
+            song_data = _get_song_info_with_urls(song_info)
+            response_data = _create_response("success", 200, "Song deleted successfully", {"song_info": song_data})
+            return response_data, 200
         else:
-            response_data = {
-                "status": "error",
-                "code": 404,
-                "message": "Song not found",
-                "song_info": None
-            }
-            return jsonify(response_data), 404
+            response_data = _create_response("error", 404, "Song not found", {"song_info": None})
+            return response_data, 404
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
     
-
 @app.route('/search_songs', methods=['GET'])
 def search_songs():
     try:
@@ -358,8 +274,8 @@ def search_songs():
         search_term = request.args.get('q')
         
         if not search_term:
-            return jsonify({"message": "Missing 'q' parameter in the request."}), 400
-        
+            return _create_response("error", 400, "Missing 'q' parameter in the request.")
+
         headers = {"Content-Type": "application/json"}
         # Use Elasticsearch to search for songs with the given search term
         search_results = elasticsearch_client.search(
@@ -382,33 +298,51 @@ def search_songs():
         for song_id in song_ids:
             song_info = songs_collection.find_one({"_id": ObjectId(song_id)})
             if song_info:
-                music_style_id = song_info.get("music_style_id")
-                style_info = music_style_collection.find_one({"_id": ObjectId(music_style_id)})
-                music_style_name = style_info.get("style_name") if style_info else "Unknown"
+                song_data = _get_song_info_with_urls(song_info)
+                matching_songs.append(song_data)
 
-                matching_songs.append({
-                    "song_info_id": str(song_info["_id"]),
-                    "song_title": song_info["song_title"],
-                    "song_text": song_info["song_text"],
-                    "description": song_info.get("description", ""),
-                    "keywords": song_info.get("keywords", ""),
-                    "planned_date": song_info.get("logical_date", ""),
-                    "music_style": music_style_name
-                })
+        response_data = _create_response("success", 200, "Songs retrieved successfully", {"matching_songs": matching_songs})
 
-        response_data = {
-            "status": "success",
-            "code": 200,
-            "message": "Songs retrieved successfully.",
-            "matching_songs": matching_songs
-        }
-
-        return jsonify(response_data), 200
+        return response_data, 200
 
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
-        return jsonify({"message": "An internal server error occurred"}), 500
+        response_data = _create_response("error", 500, "An internal server error occurred")
+        return response_data
     
+def _create_response(status, code, message, data=None):
+    response_data = {
+        "status": status,
+        "code": code,
+        "message": message,
+        "data": data
+    }
+    return jsonify(response_data), code
+
+def _get_song_info_with_urls(song_info):
+    melody_url = f"{LYRIC_WAVE_STREAMING_SERVICE_URL}/stream_melody/{song_info['_id']}"
+    voice_url = f"{LYRIC_WAVE_STREAMING_SERVICE_URL}/stream_voice/{song_info['_id']}"
+    song_url = f"{LYRIC_WAVE_STREAMING_SERVICE_URL}/stream_song/{song_info['_id']}"
+    image_url = f"{LYRIC_WAVE_STREAMING_SERVICE_URL}/show_image/{song_info['_id']}"
+    
+    music_style_id = song_info.get("music_style_id")
+    style_info = music_style_collection.find_one({"_id": ObjectId(music_style_id)})
+    music_style_name = style_info.get("style_name") if style_info else "Unknown"
+
+    song_data = {
+        "song_info_id": str(song_info["_id"]),
+        "song_title": song_info["song_title"],
+        "song_text": song_info["song_text"],
+        "description": song_info.get("description", ""),
+        "keywords": song_info.get("keywords", ""),
+        "planned_date": song_info.get("logical_date", ""),
+        "music_style": music_style_name,
+        "melody_url": melody_url,
+        "voice_url": voice_url,
+        "image_url": image_url
+    }
+    
+    return song_data
 
 # Start the Flask application if this script is executed directly
 if __name__ == '__main__':
